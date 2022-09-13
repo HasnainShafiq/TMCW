6c991cc9eefaeaa4030fb63551f9dc86ddcc55a6
Uncreative-creativity <31780746+Uncreative-creativity@users.noreply.github.com>
2022-09-13 16:27:24 +0100

    New site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uncreative-creativity.github.io',
-  base: '/The-Moving-Car-Wash',
   vite: {
     ssr: {
       external: ["svgo"],
